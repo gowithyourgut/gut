6703349f6198b1cee10702f728d08ecfc6f925dc
Ryan Reynolds <reynoldsryanscott@gmail.com>
2016-02-15 17:05:15 -0800

    [chore] heroku deploy

--- server/config.js
+++ server/config.js
@@ -2,5 +2,6 @@ module.exports = {
   google_api_key: 'AIzaSyDMpgGvo_py3fTpnwdC2DDVX4UDnkWC9eY',
   uberClientId: 'Gu337MzP2yR7FUyfKLLX7wDz2p-MJvld',
   uberServerToken: 'WSkq0fXZjd9u9NkPHhNMNWfSps2oKqBBZmvH_4Fv',
+  mongoURI: 'mongodb://admin:5tgb4rfv3edc@ds031822.mongolab.com:31822/snappea',
   jwtSecret: 'bananaheroku'
 };
